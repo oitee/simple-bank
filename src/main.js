58f149bd70a28a0c2261e16515163d14a00dd8aa
oitee <oitee.codes@gmail.com>
2022-02-15 23:13:58 +0530

    Add functionality to support withdrawals and deposits

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as model from "./model.js";
 
-function createAccount(name) {//! Add initial deposit amount
+function createAccount(name) {
+  //! Add initial deposit amount
   const accountId = model.createAccount(name);
   if (accountId) {
     return accountId;
@@ -10,41 +11,59 @@ function createAccount(name) {//! Add initial deposit amount
 
 async function deposit(accountNo, amount) {
   if (confirmDepositAmount(amount)) {
-    return await model.deposit(accountNo, amount);
+    return await model.singleAccountTransaction(accountNo, amount, "deposit");
   } else {
-    return `Error: Deposit amount needs to between INR 500 and INR 50,000: ${amount}`;
+    return `Error: Deposit amount needs to between INR 500 and INR 50,000 per transaction`;
   }
 }
 
-function confirmDepositAmount(amount){
-    return amount >= 500 && amount <= 50000; 
+async function withdraw(accountNo, amount) {
+  if (confirmWithdrawalAmount(amount)) {
+    return await model.singleAccountTransaction(accountNo, amount, "withdraw");
+  } else {
+    `Error: Withdrawal amount needs to be INR 1,000 and INR 25,000 per transaction`;
+  }
+}
+
+function confirmDepositAmount(amount) {
+  return amount >= 500 && amount <= 50000;
+}
+
+function confirmWithdrawalAmount(amount) {
+  return amount >= 1000 && amount <= 25000;
 }
 
 const holder1 = "Alice" + Math.random();
 const holder2 = "Bob" + Math.random();
 
-async function test(){
-    const accountNo1 = await createAccount(holder1);
-    const accountNo2 = await createAccount(holder2); 
-    console.log(accountNo1);
-    console.log(typeof accountNo1 === "number");
-    console.log(accountNo2);
-    console.log(typeof accountNo2 === "number");
+async function test() {
+  const accountNo1 = await createAccount(holder1);
+  const accountNo2 = await createAccount(holder2);
+  console.log(accountNo1);
+  console.log(typeof accountNo1 === "number");
+  console.log(accountNo2);
+  console.log(typeof accountNo2 === "number");
 
-    console.log(`Account1 deposit with 10000`)
-     console.log(await deposit(accountNo1, 10000));
-    console.log(`Account2 deposit with 10000`)
-    console.log(await deposit(accountNo2, 10000));
-    console.log("More than 3 deposit transactions in a day");
-    console.log(await deposit(accountNo1, 10000));
-    console.log(await deposit(accountNo1, 10000));
-    console.log(await deposit(accountNo1, 10000));
-    
+  console.log(`Account1 deposit with 10000`);
+  console.log(await deposit(accountNo1, 10000));
+  console.log(`Account2 deposit with 10000`);
+  console.log(await deposit(accountNo2, 10000));
+  console.log("More than 3 deposit transactions in a day");
+  console.log(await deposit(accountNo1, 10000));
+  console.log(await deposit(accountNo1, 10000));
+  console.log(await deposit(accountNo1, 10000));
 
-    
+  console.log(`--- Withdrawals`);
+  console.log(`Account1 withdraws 1000`);
+  console.log(await withdraw(accountNo1, 1000))
+  console.log(`Account2 withdraws 1000`);
+  console.log(await withdraw(accountNo2, 1000))
+  console.log("More than 3 withdrawl transactions in a day");
+  console.log(await withdraw(accountNo1, 1000));
+  console.log(await withdraw(accountNo1, 1000));
+  console.log(await withdraw(accountNo1, 1000));
+
+  
 }
 
-test()
-
-
-
+test();
--- src/model.js
+++ src/model.js
@@ -19,33 +19,46 @@ export async function createAccount(name) {
   }
 }
 
-export async function deposit(accountNo, amount) {
+
+export async function singleAccountTransaction(accountNo, amount, transactionType) {
   const client = await db.pool.connect();
+  const dbQuery = getSingleTransactionQuery(transactionType);
+  if(!dbQuery){
+      return `For single account transactions, please use 'deposit' or 'withdraw'`
+  }
   try {
-    const transactionId = await findTransactionId(client, "deposit");
+    const transactionId = await findTransactionId(client, transactionType);
     if (await confirmAccount(client, accountNo)) {
-     
-     
-      //check the total number of deposits
       if (await confirmTransactionLimit(client, transactionId, accountNo)) {
         await client.query(`BEGIN ISOLATION LEVEL REPEATABLE READ`);
-        const res1 = await client.query(
-          "UPDATE account SET balance = balance + $1::int WHERE id = $2 AND (balance + $1::int) < 100000 RETURNING balance",
+        const res = await client.query(
+          dbQuery,
           [amount, accountNo]
         );
-        if (res1.rows.length === 0) {
+        if (res.rows.length === 0) {
           //! Investigate errors here (optional)
           client.query(`ROLLBACK`);
-          return `Transaction Unsucessful. The resultant balance will exceed INR 1000`;
+        if(transactionType === "deposit"){
+            return `Transaction Unsucessful. The resultant balance will exceed INR 1000`;
+        }
+        return `Transaction Unsucessful. The resultant balance will reduce INR 0`;
         }
         await client.query(
           "INSERT INTO ledger (operation, account, amount) VALUES ($1, $2, $3)",
           [transactionId, accountNo, amount]
         );
         await client.query("COMMIT");
-        return `Transaction successful. INR ${amount} deposited in ${accountNo}`;
+        if(transactionType === "deposit"){
+            return `Transaction successful. INR ${amount} deposited in ${accountNo}`;
+        }
+        return `Transaction successful. INR ${amount} withdrawn from ${accountNo}`;
+        
       } else {
-        return `Error: Exceeded the total number of permissible deposits for the day`;
+          if(transactionType === "deposit"){
+            return `Error: Exceeded the total number of permissible deposits for the day`;
+          }
+          return `Error: Exceeded the total number of permissible withdrawals for the day`;
+        
       }
     } else {
       return `Error: Account does not exist: ${accountNo}`;
@@ -57,6 +70,18 @@ export async function deposit(accountNo, amount) {
     await client.release();
   }
 }
+
+function getSingleTransactionQuery(transactionType){
+    switch(transactionType){
+        case "deposit":
+            return   "UPDATE account SET balance = balance + $1 WHERE id = $2 AND (balance + $1) < 100000 RETURNING balance"
+        case "withdraw":
+            return "UPDATE account SET balance = balance - $1 WHERE id = $2 and (balance - $1) > 0 RETURNING balance"
+        default:
+            return null;
+    }
+}
+
 
 async function confirmAccount(client, accountNo) {
   const res = await client.query("SELECT * FROM account WHERE id = $1", [
@@ -78,10 +103,22 @@ async function findTransactionId(client, transactionType) {
   return res.rows[0].id;
 }
 
-async function confirmTransactionLimit(client, transactionId, accountNo){
-    const currentTime = new Date();
-    const lastMidnight = currentTime.setUTCHours(0, 0, 0, 0) / 1000;
+async function confirmTransactionLimit(client, transactionId, accountNo) {
+  const currentTime = new Date();
+  const lastMidnight = currentTime.setUTCHours(0, 0, 0, 0) / 1000;
 
-    const res = await client.query("SELECT * FROM ledger WHERE operation = $1 AND account = $2 AND created_at > to_timestamp($3)", [transactionId, accountNo, lastMidnight]);
-    return res.rows.length < 3;
+  const res = await client.query(
+    "SELECT * FROM ledger WHERE operation = $1 AND account = $2 AND created_at > to_timestamp($3)",
+    [transactionId, accountNo, lastMidnight]
+  );
+  return res.rows.length < 3;
+}
+
+
+
+function test(){
+
+console.log(getSingleTransactionQuery("deposit"))
+console.log(getSingleTransactionQuery("withdraw"))
+console.log(getSingleTransactionQuery("www"));
 }
